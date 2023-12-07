f551e5349f6be4d96384ee7388693d2682983781
e.skrupskaite <e.skrupskaite@DESKTOP-OUK37GL>
2023-12-07 13:05:28 +0200

    added function to add student to department

--- University/Controllers/StudentasController.cs
+++ University/Controllers/StudentasController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using University.Interfaces;
 
 namespace University.Controllers
 {
+    [ApiController]
+    [Route("[controller]/[action]")]
     public class StudentasController : Controller
     {
-        public IActionResult Index()
+        private readonly IStudentasService _studentasService;
+
+        public StudentasController(IStudentasService studentasService)
         {
-            return View();
+            _studentasService = studentasService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddStudentToDepartment([FromBody] int student_id, int department_id)
+        {
+            return Ok(_studentasService.AddStudentToDepartment(student_id, department_id));
         }
     }
 }
--- University/Services/StudentasService.cs
+++ University/Services/StudentasService.cs
@@ -4,5 +4,15 @@ namespace University.Services
 {
     public class StudentasService : IStudentasService
     {
+        private readonly IStudentasRepository _studentasRepository;
+        
+        public StudentasService(IStudentasRepository studentasRepository)
+        {
+            _studentasRepository = studentasRepository;
+        }
+        public int AddStudentToDepartment(int student_id, int department_id)
+        {
+            return _studentasRepository.AddStudentToDepartment(student_id, department_id);
+        }
     }
 }
